ff53784fdf67a27e945229f75891ec7a642d30af
B Gowtham <gowtham@Gowtham.domain.name>
2022-12-21 10:56:58 +0530

    Design patterns start

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
